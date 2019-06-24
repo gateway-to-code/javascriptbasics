e69303bd2296a334b9d68d1e7172ab0d5ad56d00
ctod <curiouscarine@Carines-MacBook-Pro.local>
2019-06-23 23:14:05 -0400

    added question and solution

---
+++ fizz-buzz.js
@@ -0,0 +1,31 @@
+/*
+
+Create a function that takes a number as a parameter and returns fizz if the number is divisible by 3 
+and buzz if the number is divisible by 5. Your function should output as follows 
+
+fizzBuzz(45) // fizz
+fizzBuzz(7) //flat soda
+fizzBuzz("hello") // please enter a number 
+fizzBuzz(20) // 
+*/ 
+
+
+/*
+
+Create a function that 
+
+*/ 
+
+function fizzBuzz(number) {
+	if(typeof number === 'number') {
+		if((number%3 === 0)&& typeof number === 'number') {
+			return document.write('fizz');
+		} else if((number%5 === 0) && typeof number === 'number') {
+			return document.write('buzz');
+		} else {
+			return document.write('Flat soda');
+		}
+	} else {
+		return document.write('Please enter a number');
+	}
+}
