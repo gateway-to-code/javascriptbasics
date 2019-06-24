0781290d6ef692a83bbb36f9690fcb1996396b52
ctod <curiouscarine@Carines-MacBook-Pro.local>
2019-06-23 23:57:38 -0400

    added shuffle deck question

---
+++ shuffledeck.js
@@ -0,0 +1,12 @@
+/*
+
+Create a function that takes a deck of cards in one order and returns the cards is a different order 
+
+
+
+Input 										Output
+var deck = [1,2,3,4,5,6,7,8,9,10] 			updated deck				
+shuffleDeck(deck)                           // []
+
+*/ 
+
