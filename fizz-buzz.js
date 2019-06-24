a292027a02a0ac1219bfcad88ea436673804a188
ctod <curiouscarine@Carines-MacBook-Pro.local>
2019-06-23 23:40:30 -0400

    Updated problem statement

---
+++ fizz-buzz.js
@@ -0,0 +1,20 @@
+/*
+
+Create a function that takes a number as a parameter and returns fizz if the number is divisible by 3 
+and buzz if the number is divisible by 5. If it is divisible by 3 and 5 it should return fizzbuzz. 
+Your function should output as follows 
+
+Input 	           Output 
+fizzBuzz(45)      // "fizz"
+fizzBuzz(20)      // "buzz"
+fizzBuzz(15)      //"fizzbuzz"
+
+Extra Credit 
+your function should return "flat soda" if the input is not a number. Your function should return "please enter a number" 
+if the input is not a number.
+
+Input               Output
+fizzBuzz(7)         //"flat soda"
+fizzBuzz("hello")   // "please enter a number"
+*/ 
+
