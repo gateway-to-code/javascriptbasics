431039a7f9c717bc9b710c08cada9074371aa107
ctod <curiouscarine@Carines-MacBook-Pro.local>
2019-07-02 06:38:23 -0400

    Update shuffledeck.js

--- shuffledeck.js
+++ shuffledeck.js
@@ -6,7 +6,7 @@ Create a function that takes a deck of cards in one order and returns the cards 
 
 Input 										Output
 var deck = [1,2,3,4,5,6,7,8,9,10] 			updated deck				
-shuffleDeck(deck)                           // []
+shuffleDeck(deck)                           // [1, 10, 9, 7, 4, 6, 3, 8, 2, 5]
 
 */ 
 
